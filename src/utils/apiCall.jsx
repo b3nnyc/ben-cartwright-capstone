9f3e50191a86291602d9d06abe747faab12b775e
Ben Cartwright <ben.cartwright@outlook.com>
2022-03-27 16:16:41 -0400

    Added more api routes

--- src/utils/apiCall.jsx
+++ src/utils/apiCall.jsx
@@ -2,15 +2,20 @@ import axios from "axios";
 
 const proxyApi = "https://crossorigin.me/"
 const quoteApi = `${proxyApi}https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=`
-const databaselocation = null
+const portfolioDB = null
+const watchlistDB = null
 
 export default {
     getHomeStockData: () => axios.get(`${quoteApi}QQQ`, {headers: {'X-API-KEY':'TFFJcis3fU2ChGJNaKoe7ZXrFktxOmu7FhsQ1f60'}}),
     getStockData: (symbol) => axios.get(`${quoteApi}${symbol}`, {headers: {'X-API-KEY':'TFFJcis3fU2ChGJNaKoe7ZXrFktxOmu7FhsQ1f60'}}),
-    getPortfolioList: () => axios.get(`${databaselocation}`),
-    addPortfolioItem: (symbol, shares, avgPrx) => axios.post(`${databaselocation}`, {
+    getPortfolioList: () => axios.get(`${portfolioDB}`),
+    addPortfolioItem: (symbol, shares, avgPrx) => axios.post(`${portfolioDB}`, {
         symbol: symbol,
         shares: shares, 
         avgPrx: avgPrx 
+    }),
+    getWatchlist: () => axios.get(`${watchlistDB}`),
+    addWatchlist: (symbol) => axios.post(`${watchlistDB}`, {
+        symbol: symbol
     })
 }
