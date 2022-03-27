973e615d13ffe3803050462a3cf87e363a4720d1
Ben Cartwright <ben.cartwright@outlook.com>
2022-03-27 17:01:07 -0400

    Got API calls working properly

--- src/utils/apiCall.jsx
+++ src/utils/apiCall.jsx
@@ -1,13 +1,20 @@
 import axios from "axios";
 
-const proxyApi = "https://crossorigin.me/"
-const quoteApi = `${proxyApi}https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=`
+const quoteApi = `https://yh-finance.p.rapidapi.com`
+const homeQuotes = `/market/v2/get-quotes?region=US&symbols=qqq%2Cspy`
+const singleQuote = `/market/v2/get-quotes?region=US&symbols=`
+const trending = `/market/get-trending-tickers`
+const recommendations = `/stock/v2/get-recommendations`
+const apiKey = `194a67eae9mshafa1551f18bdb31p1e1bacjsnd6211d84a21e`
+const apiHost = `yh-finance.p.rapidapi.com`
 const portfolioDB = null
 const watchlistDB = null
 
 export default {
-    getHomeStockData: () => axios.get(`${quoteApi}QQQ`, {headers: {'X-API-KEY':'TFFJcis3fU2ChGJNaKoe7ZXrFktxOmu7FhsQ1f60'}}),
-    getStockData: (symbol) => axios.get(`${quoteApi}${symbol}`, {headers: {'X-API-KEY':'TFFJcis3fU2ChGJNaKoe7ZXrFktxOmu7FhsQ1f60'}}),
+    getHomeStockData: () => axios.get(`${quoteApi}${homeQuotes}`, {headers: {'X-RapidAPI-Key':`${apiKey}`, 'X-RapidAPI-Host': `${apiHost}`, }}),
+    getTrending: () => axios.get(`${quoteApi}${trending}`, {headers: {'X-RapidAPI-Key':`${apiKey}`, 'X-RapidAPI-Host': `${apiHost}`}}),
+    getRecommended: (symbol) => axios.get(`${quoteApi}${recommendations}?symbol=${symbol}`, {headers: {'X-RapidAPI-Key':`${apiKey}`, 'X-RapidAPI-Host': `${apiHost}`}}),
+    getSingleStock: (symbol) => axios.get(`${quoteApi}${singleQuote}${symbol}`, {headers: {'X-RapidAPI-Key':`${apiKey}`, 'X-RapidAPI-Host': `${apiHost}`}}),
     getPortfolioList: () => axios.get(`${portfolioDB}`),
     addPortfolioItem: (symbol, shares, avgPrx) => axios.post(`${portfolioDB}`, {
         symbol: symbol,
@@ -19,3 +26,6 @@ export default {
         symbol: symbol
     })
 }
+
+// /auto-complete for search bar
+// /stock/v3/get-chart for chart info
